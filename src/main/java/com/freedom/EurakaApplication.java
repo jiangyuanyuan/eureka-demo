dd29d2016ed377df4e2be0a018726a1a1eb5bbfc
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-06-18 21:21:28 +0800

    Initial commit

---
+++ src/main/java/com/freedom/EurakaApplication.java
@@ -0,0 +1,14 @@
+package com.freedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaApplication.class, args);
+	}
+}
